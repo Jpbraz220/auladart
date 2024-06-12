421f1f0a61c6dec27bdccbd90372f26533ac9da8
Jp <joao.braz@ba.estudante.senai.br>
2024-06-12 16:34:57 -0300

    Acabou o uso dos exemplos de frutas

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,4 +1,6 @@
 void main(List<String> arguments) {
+//Aula 1 -----------------------------------------------------------------------
+
   int idade = 31;
   double altura = 1.71;
   bool geek = true;
@@ -37,4 +39,89 @@ void main(List<String> arguments) {
 
   //for (i = 0; i <= 10; i++) {
   //  print('$i');}
+
+  //Aula 2 ---------------------------------------------
+
+  String fruta = "Laranja";
+  double pesoDaFruta = 50.4;
+  int tempoDeColhida = 31;
+  int tempoParaAmadurecer = 20;
+  String frase2 = criarFrase(
+      fruta: fruta,
+      pesoDaFruta: pesoDaFruta,
+      tempoDeColhida: tempoDeColhida,
+      tempoParaAmadurecer: tempoParaAmadurecer);
+  print(frase2);
+  Fruit fruta01 =
+      Fruit(fruta, pesoDaFruta, tempoDeColhida, tempoParaAmadurecer);
+  fruta01.estaMadura(tempoDeColhida, tempoParaAmadurecer);
+  String nivelDeAzedo = "médio";
+  Citrica laranja = Citrica(
+      fruta, pesoDaFruta, tempoDeColhida, tempoParaAmadurecer, nivelDeAzedo);
+  laranja.fazersuco(pesoDaFruta);
+  laranja.verQuantoCabe();
+  laranja.verQuantoCusta();
+}
+
+String criarFrase(
+    {required String fruta,
+    required double pesoDaFruta,
+    required int tempoDeColhida,
+    required int tempoParaAmadurecer}) {
+  String result =
+      'A $fruta pesa $pesoDaFruta gramas, foi colhida a $tempoDeColhida dias e precisa de $tempoParaAmadurecer, logo ela está madura!';
+  return (result);
+}
+
+class Fruit implements Embalar {
+  String nome;
+  double peso;
+  int tempoDeColhida;
+  int tempoParaAmadurecer;
+  bool? madura;
+  Fruit(this.nome, this.peso, this.tempoDeColhida, this.tempoParaAmadurecer,
+      {this.madura});
+  estaMadura(int tempoDeColhida, int tempoParaAmadurecer) {
+    madura = tempoDeColhida >= tempoParaAmadurecer;
+    String texto = '';
+    if (madura = false) {
+      texto = ' não';
+    }
+    print("A $nome$texto está madura!");
+  }
+
+  @override
+  String verQuantoCabe() {
+    //é necessário 10kg para encher um saco pra vender (CABEÇA, Vozes da. 2024)
+    String x = (10000 / peso).toStringAsFixed(2);
+    print("É necessário $x dessa fruta para encher a saca");
+    return x;
+  }
+
+  @override
+  void verQuantoCusta() {
+    //essa fruta custa 20 centavos por fruta
+    double x = (10000 / peso);
+    String y = (x * 0.2).toStringAsFixed(2);
+    print('A saca dessa fruta custa $y');
+  }
+}
+
+class Citrica extends Fruit {
+  String nivelDeAzedo;
+  Citrica(String nome, double peso, int tempoDeColhida, int tempoParaAmadurecer,
+      this.nivelDeAzedo)
+      : super(nome, peso, tempoDeColhida, tempoParaAmadurecer);
+
+  void fazersuco(double peso) {
+    //é necessário 500g para fazer um suco (CABEÇA, Vozes da. 2024)
+    String x = (500 / peso).toStringAsFixed(2);
+    print("É necessário $x dessa fruta para fazer suco");
+  }
+}
+
+abstract class Embalar {
+  void verQuantoCabe();
+
+  void verQuantoCusta();
 }
